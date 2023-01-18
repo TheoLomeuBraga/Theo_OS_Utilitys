f90116be87d75693859eb5d49febe3dd80bf172a
TheoLomeuBraga <theolomeu@gmail.com>
2023-01-18 17:49:23 -0300

    a

--- src/linux/theo_os_library.cpp
+++ src/linux/theo_os_library.cpp
@@ -3,4 +3,5 @@
 #include <theo_os_library.h>
 
 
-void print_test(){std::cout << "This is a Thest" << std::endl;}
+void print_test(){std::cout << "this is a test" << std::endl;}
+
--- src/theo_os_library.h
+++ src/theo_os_library.h
@@ -2,3 +2,4 @@
 
 
 void print_test();
+
